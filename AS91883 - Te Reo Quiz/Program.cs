f0a85795033a275d8e901b19d2f00563e943b9fe
William Purdon <ac117097@avcol.school.nz>
2022-03-18 14:13:56 +1300

    Got level selector working, started on questions.

--- AS91883 - Te Reo Quiz/Program.cs
+++ AS91883 - Te Reo Quiz/Program.cs
@@ -1,32 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string selection;
+int selection, q1d1;
 //introduction 
-Console.WriteLine("Hello there user! Welcome to this Te Reo Maori quiz.");
-Console.WriteLine("This quiz will test your Te Reo Maori knowlege by asking you multichoice questions and word answer questions. After you answer these questions, the program will calculate and display your skill level.");
-//quiz level selector
-Console.WriteLine("\n\nWhich quiz mode would you like to try? Please type 'Beginner', 'Intermediate', or 'Advanced' depending on which quiz level you would like to take.");
-selection = Convert.ToString(Console.ReadLine()); 
-string selectionResult = selection.ToLower(); //converts the string that the user entered into lowercase so that it's easier to recongnize
-if(selectionResult != "beginner" || selectionResult != "intermediate" || selectionResult != "advanced")
-{
 
-}
+Console.WriteLine("Kia Ora user! Welcome to this Te Reo Maori quiz.");
+Console.WriteLine("This quiz will test your Te Reo Maori knowlege by asking you 15 multichoice questions. After you answer these questions, the program will calculate and display your skill level.");
+
+//quiz level selector
+Console.WriteLine("\n\nDifficulty level:");
+Console.WriteLine("\n\n1 = Beginner" + "\n\n2 = Intermediate" + "\n\n3 = Advanced");
+Console.Write("\n\nPlease choose a difficulty level by typing the number next to the level you would like to do:");
+while (!int.TryParse(Console.ReadLine(), out selection) || selection <= 0 || selection >= 4)
 {
-    if (selectionResult == "beginner" || selectionResult == "beginer" || selectionResult == "beginnner" || selectionResult == "begginer") 
-    {
-        Console.WriteLine("You choose Beginner");
-    }
-    else if (selectionResult == "intermediate" || selectionResult == "intermedtate" || selectionResult == "intermdiate" || selectionResult == "intermdiate")
-    {
-        Console.WriteLine("You choose Intermediate");
-    }
-    else if (selectionResult == "advanced")
-    {
-        Console.WriteLine("You choose Advanced");
-    }
-    else
-    {
-        Console.Write("You entered an invalid quiz type or you misspelt the name of the quiz level. Please try again.");
-    }
+    Console.Write("\nPlease only enter a valid number option: ");
 }
+switch (selection)
+{
+    case 1:
+        Console.WriteLine("\n\nYou choose the Beginner quiz! Begin answering the questions.");
+        Console.Write("\n\nQuestion 1: How do you say 'hello' in Maori?");
+        Console.WriteLine("\n\n1) Haere rā \n2) Tēnā koe \n3) Kia Ora \n4) Tēnā koutou");
+        q1d1 = Convert.ToInt32(Console.ReadLine());
+        break;
+
+    case 2:
+        Console.WriteLine("\n\nYou choose the Intermediate quiz! Begin answering the questions.");
+        break;
+
+    case 3:
+        Console.WriteLine("\n\nYou choose the Advanced quiz! Begin answering the questions.");
+        break;
+}
+
+
+
+
+
+
 
